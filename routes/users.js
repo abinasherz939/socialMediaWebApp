948ccf5303a8de98f74675d60bf911127343e430
Abinash939 <abinashchoudharyasansol@gmail.com>
2022-03-28 16:45:52 +0530

    added sign in using passport.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const usersController = require('../controllers/users_controller');
 
-const usersConrtoller = require('../controllers/users_controller');
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 
-router.get('/profile', usersConrtoller.profile);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
+
+
+router.post('/create', usersController.create);
+
+
+
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
+
+
+// // this is if we are not using passposrt.js
+// router.post('/create-session', usersController.createSession);
 
 
 module.exports = router;
